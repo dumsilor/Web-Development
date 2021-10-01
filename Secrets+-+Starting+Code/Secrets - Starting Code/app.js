85eed5e8ebe715bb808b8c6e0781c9942455e71e
Mosiur Rahman <dum.silor@gmail.com>
2021-10-01 20:41:32 +0600

    Web Dev authentication and security, using bcypt (need to remove the comment for bcrypt)

--- Secrets+-+Starting+Code/Secrets - Starting Code/app.js
+++ Secrets+-+Starting+Code/Secrets - Starting Code/app.js
@@ -5,7 +5,17 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const e = require("express");
-const encrypt = require("mongoose-encryption");
+//const encrypt = require("mongoose-encryption");
+//const md5 = require("md5"); 
+//const bcrypt = require("bcrypt");
+//const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const { Session } = require("express-session");
+
+
+
 
 const app = express();
 
@@ -14,6 +24,14 @@ const secret = process.env.secret;
 app.use(express.static("public"));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
 
@@ -22,11 +40,16 @@ const userSchema = new mongoose.Schema({
     password: String
 })
 
+userSchema.plugin(passportLocalMongoose);
 
-userSchema.plugin(encrypt, { secret: secret, encryptedFields:['password']});
+//userSchema.plugin(encrypt, { secret: secret, encryptedFields:['password']});
 
 
 const User = new mongoose.model("user",userSchema)
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req,res){
     res.render("home")
@@ -41,9 +64,10 @@ app.get("/register", function(req,res){
 })
 
 app.post("/register", function(req,res){
-    const newUser = new User ({
+    bcrypt.hash(req.body.password, saltRounds, function(err,hash){
+        const newUser = new User ({
         username: req.body.username,
-        password: req.body.password
+        password: hash
     });
     newUser.save(function(err){
         if (err){
@@ -52,22 +76,27 @@ app.post("/register", function(req,res){
             res.render("secrets");
         }
     });
+    })
 });
 
 app.post("/login",function(req,res){
     const userName = req.body.username;
     const password = req.body.password;
+    console.log(password)
     User.findOne({username: userName}, function(err,foundUser){
         if (err){
             console.log(err)
         } else {
             if (foundUser){
-                if (foundUser.password === password){
-                    res.render("secrets");
+                bcrypt.compare(password,foundUser.password, function(err,result){
+                    if (result === true){
+                        res.render("secrets");
+                    }
+                });
+                
                 }
             }
-        }
-    });
+        });
 });
 
 app.listen(3000,function(){
