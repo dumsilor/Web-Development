99c5256dddaf5acd321fcf39370d2a65d48a4ed3
dumsilor <68141943+dumsilor@users.noreply.github.com>
2021-10-05 10:02:49 +0600

    Update app.js
    Updated app.js file

--- ic_login_page/app.js
+++ ic_login_page/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static('public'));
+
 
 app.get("/",function(req,res){
-    res.send("Hello");
+    res.sendFile(__dirname +"/public/index.html");
 });
 
 app.listen(3030,function(){
